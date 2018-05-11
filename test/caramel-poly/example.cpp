9ff42226b04206195d8d0411b1439a38561a3fe8
Mikołaj Radwan <mikoszrrr@gmail.com>
2018-05-11 22:38:47 +0200

    Example has full basic functionality (looks like)

--- test/caramel-poly/example.cpp
+++ test/caramel-poly/example.cpp
@@ -11,32 +11,38 @@ namespace /* anonymous */ {
 using namespace caramel_poly;
 using namespace caramel_poly::storage;
 
-template <int... MethodIds>
+template <int ID_PARAM, class MethodType>
+struct Entry {
+	static constexpr auto ID = ID_PARAM;
+	using Method = MethodType;
+};
+
+template <class... Entries>
 class MP;
 
-template <int METHOD_ID, int... MethodIds>
-class MP<METHOD_ID, MethodIds...> : MP<MethodIds...> {
+template <int ID, class Head, class... Tail>
+class MP<Entry<ID, Head>, Tail...> : MP<Tail...> {
 public:
 
-	template <class Method, class... Methods>
-	MP(Method&& method, Methods&&... methods) :
-		MP<MethodIds...>(std::forward<Methods>(methods)...),
-		method_(std::forward<Method>(method))
+	template <class... TailMethods>
+	constexpr MP(Head head, TailMethods... tail) :
+		MP<Tail...>(tail...),
+		method_(head)
 	{
 	}
 
-	template <int ID>
-	const void* const get() const {
-		if constexpr (ID == METHOD_ID) {
+	template <int GET_ID>
+	constexpr auto get() const {
+		if constexpr (GET_ID == ID) {
 			return method_;
 		} else {
-			return MP<MethodIds...>::get<ID>();
+			return MP<Tail...>::get<GET_ID>();
 		}
 	}
 
 private:
 
-	const void* const method_;
+	Head method_;
 
 };
 
@@ -44,33 +50,36 @@ template <>
 class MP<> {
 public:
 
-	template <int ID>
-	const void* get() const {
+	template <int GET_ID>
+	constexpr auto get() const {
 		throw std::out_of_range("No such function");
 	}
 
 };
 
-template <int... MethodIds>
+template <class... Methods>
+constexpr auto makeMethodMap(Methods... methods) {
+	return MP<Methods...>(methods...);
+}
+
+template <class... Entries>
 class VT {
 public:
 
 	template <class... Methods>
-	VT(Methods&&... methods) :
-		mp_(std::forward<Methods>(methods)...)
+	constexpr VT(Methods... methods) :
+		mp_(methods...)
 	{
 	}
 
-	template <int fun, class ReturnType, class... Args>
+	template <int ID, class ReturnType, class... Args>
 	ReturnType invoke(Args&&... args) const {
-		// this is a bug: due to args being passed by a universal-reference, we cast a function
-		// that takes a by-value argument to a function taking a rvalue-reference.
-		return (*static_cast<ReturnType (*)(Args...)>(mp_.get<fun>()))(std::forward<Args>(args)...);
+		return (*mp_.get<ID>())(std::forward<Args>(args)...);
 	}
 
 private:
 
-	MP<MethodIds...> mp_;
+	MP<Entries...> mp_;
 
 };
 
@@ -84,17 +93,24 @@ bool polyPoly(const void* self, int arg) {
 	return static_cast<const T*>(self)->polymorphicFunction(arg);
 }
 
+struct VTConcept {
+	using VTType = VT<
+		Entry<0, void (*)(const void*)>,
+		Entry<1, bool (*)(const void*, int)>
+		>;
+};
+
 template <class T>
-VT<0, 1> makeVT() {
-	return VT<0, 1>(&polyDes<T>, &polyPoly<T>);
-}
+struct VTMapping {
+	static constexpr auto vt = VTConcept::VTType(&polyDes<T>, &polyPoly<T>);
+};
 
 class Poly final {
 public:
 
 	template <class T>
 	Poly(T model) :
-		vtable_(makeVT<T>()),
+		vtable_(VTMapping<T>::vt),
 		storage_(std::move(model))
 	{
 	}
@@ -106,7 +122,7 @@ public:
 
 private:
 
-	VT<0, 1> vtable_;
+	VTConcept::VTType vtable_;
 
 	SBORemote<SharedRemote> storage_;
 
